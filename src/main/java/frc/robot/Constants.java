089e338ddbac8e676850ca14f834bf2f31d39cc5
Doug <dougdliu@gmail.com>
2023-07-11 00:42:11 -0700

    1. Added a feedforward term to work against gravity

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -122,7 +122,7 @@ public final class Constants {
     public static final double kP = 1;
     public static final double kI = 0.0;
     public static final double kD = 0.0;
-    public static final double kF = 0.0;
+    public static final double kF = 0.125;
     public static final int kIzone = 0;
     public static final double kPeakOutput = 1.0;
   }
